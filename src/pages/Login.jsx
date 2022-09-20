22f3dda6abc2d281a1beffe915faf62ef3ed9afc
HongJoonHyung <sesa0802@naver.com>
2022-09-20 11:30:53 +0900

    feat : 반응형 레이아웃 구현을 위한 throttle 사용 #3

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,51 @@
-import React from 'react';
-import Layout from '../component/layout/Layout';
+import React, { useCallback, useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { throttle } from 'lodash';
 
 function Login() {
+    const [isMobile, setIsMobile] = useState(false);
+    const throttled = useCallback(throttle(newValue => newValue(), 500));
+    const handleResize = () => {
+        if (window.innerWidth <= 1023) {
+            setIsMobile(true);
+        } else {
+            setIsMobile(false);
+        }
+    };
+
+    const throttleResize = () => {
+        throttled(handleResize);
+    };
+
+    useEffect(() => {
+        handleResize();
+        window.addEventListener('resize', throttleResize);
+        return () => {
+            window.removeEventListener('resize', throttleResize);
+        };
+    }, []);
     return (
-        <Layout header sidebar>
+        <LoginLayout isMobile={isMobile}>
+            <Header>
+                <ul>
+                    <li>스톡스톡</li>
+                    <li>만든 사람들</li>
+                    <li>프로젝트 소개</li>
+                </ul>
+            </Header>
             로그인페이지입니다.
-        </Layout>
+        </LoginLayout>
     );
 }
 
 export default Login;
+
+const LoginLayout = styled.div`
+    display: flex;
+    flex-direction: ${props => (props.isMobile ? 'column' : 'row')};
+`;
+
+const Header = styled.header`
+    width: 256px;
+    height: 100%;
+`;
